6547e7e2e008c5932cc35c823888ea8f6503736a
usamahbass <basalamahusamah7@gmail.com>
2020-09-07 10:20:05 +0700

    dependencies , bulma sass setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,100 @@
-const webpack = require('webpack');
-const WebpackModules = require('webpack-modules');
-const path = require('path');
-const config = require('sapper/config/webpack.js');
-const pkg = require('./package.json');
+const webpack = require("webpack");
+const WebpackModules = require("webpack-modules");
+const path = require("path");
+const config = require("sapper/config/webpack.js");
+const pkg = require("./package.json");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const dev = mode === "development";
 
-const alias = { svelte: path.resolve('node_modules', 'svelte') };
-const extensions = ['.mjs', '.js', '.json', '.svelte', '.html'];
-const mainFields = ['svelte', 'module', 'browser', 'main'];
+const alias = { svelte: path.resolve("node_modules", "svelte") };
+const extensions = [".mjs", ".js", ".json", ".svelte", ".html"];
+const mainFields = ["svelte", "module", "browser", "main"];
 
 module.exports = {
-	client: {
-		entry: config.client.entry(),
-		output: config.client.output(),
-		resolve: { alias, extensions, mainFields },
-		module: {
-			rules: [
-				{
-					test: /\.(svelte|html)$/,
-					use: {
-						loader: 'svelte-loader',
-						options: {
-							dev,
-							hydratable: true,
-							hotReload: false // pending https://github.com/sveltejs/svelte/issues/2377
-						}
-					}
-				}
-			]
-		},
-		mode,
-		plugins: [
-			// pending https://github.com/sveltejs/svelte/issues/2377
-			// dev && new webpack.HotModuleReplacementPlugin(),
-			new webpack.DefinePlugin({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-		].filter(Boolean),
-		devtool: dev && 'inline-source-map'
-	},
+  client: {
+    entry: config.client.entry(),
+    output: config.client.output(),
+    resolve: { alias, extensions, mainFields },
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: "svelte-loader",
+            options: {
+              dev,
+              hydratable: true,
+              hotReload: false, // pending https://github.com/sveltejs/svelte/issues/2377
+            },
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    mode,
+    plugins: [
+      // pending https://github.com/sveltejs/svelte/issues/2377
+      // dev && new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode),
+      }),
+      new MiniCssExtractPlugin({
+        filename: "static/custom.css",
+        chunkFilename: "static/custom.module.css",
+      }),
+    ].filter(Boolean),
+    devtool: dev && "inline-source-map",
+  },
 
-	server: {
-		entry: config.server.entry(),
-		output: config.server.output(),
-		target: 'node',
-		resolve: { alias, extensions, mainFields },
-		externals: Object.keys(pkg.dependencies).concat('encoding'),
-		module: {
-			rules: [
-				{
-					test: /\.(svelte|html)$/,
-					use: {
-						loader: 'svelte-loader',
-						options: {
-							css: false,
-							generate: 'ssr',
-							hydratable: true,
-							dev
-						}
-					}
-				}
-			]
-		},
-		mode,
-		plugins: [
-			new WebpackModules()
-		],
-		performance: {
-			hints: false // it doesn't matter if server.js is large
-		}
-	},
+  server: {
+    entry: config.server.entry(),
+    output: config.server.output(),
+    target: "node",
+    resolve: { alias, extensions, mainFields },
+    externals: Object.keys(pkg.dependencies).concat("encoding"),
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: "svelte-loader",
+            options: {
+              css: false,
+              generate: "ssr",
+              hydratable: true,
+              dev,
+            },
+          },
+        },
+      ],
+    },
+    mode,
+    plugins: [new WebpackModules()],
+    performance: {
+      hints: false, // it doesn't matter if server.js is large
+    },
+  },
 
-	serviceworker: {
-		entry: config.serviceworker.entry(),
-		output: config.serviceworker.output(),
-		mode
-	}
+  serviceworker: {
+    entry: config.serviceworker.entry(),
+    output: config.serviceworker.output(),
+    mode,
+  },
 };
